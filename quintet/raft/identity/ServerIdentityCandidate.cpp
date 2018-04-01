efbc032c342cfe6849c47c63c2410ca6f35dd4a4
Engineev <ryw_pub@126.com>
2018-04-01 17:05:31 +0800

    launchVotesChecker

--- quintet/raft/identity/ServerIdentityCandidate.cpp
+++ quintet/raft/identity/ServerIdentityCandidate.cpp
@@ -15,3 +15,30 @@ std::vector<quintet::FutureWrapper<clmdep_msgpack::object_handle>> quintet::Serv
     }
     return res;
 }
+
+void quintet::ServerIdentityCandidate::launchVotesChecker(
+        std::vector<quintet::FutureWrapper<clmdep_msgpack::object_handle>> &&votes) {
+    voteResults.resize(votes.size());
+    for (auto &&voteResult : voteResults)
+        voteResult = std::make_shared<VoteResult>(VoteResult::Unready);
+
+    for (int i = 0; i < (int)votes.size(); ++i) {
+        votes[i].then([this, voteResult = voteResults[i]]
+                              (boost::future<RPCLIB_MSGPACK::object_handle> fut) mutable {
+            Term termReceived;
+            bool res;
+            std::tie(termReceived, res) = fut.get().as<std::pair<Term, bool>>();
+
+            // TODO: when larger term is received ...
+
+            boost::lock_guard<boost::mutex> lk(*m);
+            assert((*voteResult == VoteResult::Discarded || *voteResult == VoteResult::Unready));
+            if (*voteResult == VoteResult::Discarded)
+                return;
+            *voteResult = res ? VoteResult::Accepted : VoteResult::Rejected;
+            votesReceived += res;
+            if (votesReceived > info.srvList.size())
+                service.identityTransformer.transform(ServerIdentityNo::Leader);
+        });
+    }
+}
--- quintet/raft/identity/ServerIdentityCandidate.h
+++ quintet/raft/identity/ServerIdentityCandidate.h
@@ -2,6 +2,8 @@
 #define QUINTET_SERVERIDENTITYCANDIDATE_H
 
 #include <vector>
+#include <memory>
+#include <cassert>
 
 #include "raft/identity/ServerIdentityBase.h"
 
@@ -25,31 +27,62 @@ public:
     RPCRequestVote(Term term, ServerId candidateId,
                    std::size_t lastLogIdx, Term lastLogTerm) override {throw ;};
 
-    void leave() override {throw; }
-
     /// \breif See figure 2 of the paper
     ///
     /// 1. Increase the current term.
     /// 2. Vote for self
-    /// 3. Reset election timer
+    /// 3. Reset election timer (TODO:)
     /// 4. Send RequestVote RPCs to the other servers.
     ///    This procedure will not wait for the other servers to reply.
     void init() override {
         ++state.currentTerm;
         state.votedFor = info.local;
+        votesReceived = 1;
         // TODO: reset election timer
-        voteRequested = 1;
+        m = std::make_shared<boost::mutex>();
         launchVotesChecker(sendRequests());
     }
 
-private:
-    std::atomic<std::size_t> voteRequested{0};
+    /// \brief clean up
+    ///
+    /// 1. Discard all the remaining RequestVote RPCs
+    /// 2. Reset the shared pointers
+    void leave() override {
+        boost::unique_lock<boost::mutex> lk(*m);
+        for (auto && vote : voteResults) {
+            *vote = VoteResult::Discarded;
+            vote = nullptr;
+        }
+        // Unlock before set m to nullptr, otherwise it
+        // may happen that this thread is the last thread
+        // which hold m and set m to nullptr will release
+        // the resource before unlocking it.
+        lk.unlock();
+        m = nullptr;
+    }
 
+private:
+    enum class VoteResult { Unready, Accepted, Rejected, Discarded };
+
+    /* Why shared pointer are used here ?
+     * I think that two different term should be completely independent,
+     * which means that the operations in different term will never affect
+     * the same object. Therefore, I use this std::shared_ptr trick.
+     * At init(), new shared pointers are create and be acquired by the
+     * threads launched during this term. And at leave(), the shared pointers
+     * which stored in the object will be cleaned so that the resource will
+     * be released after all the other threads launched during this term
+     * exist.
+     */
+    std::shared_ptr<boost::mutex>             m;
+    std::vector<std::shared_ptr<VoteResult>>  voteResults;
+
+    std::atomic<std::size_t> votesReceived;
+
+private:
     std::vector<FutureWrapper<RPCLIB_MSGPACK::object_handle>> sendRequests();
 
-    void launchVotesChecker(std::vector<FutureWrapper<RPCLIB_MSGPACK::object_handle>> && votes) {
-
-    }
+    void launchVotesChecker(std::vector<FutureWrapper<RPCLIB_MSGPACK::object_handle>> && votes);
 
 }; // class ServerIdentityCandidate
 
