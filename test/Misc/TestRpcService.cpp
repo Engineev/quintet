87f54c41be8a15eb24efac3d916eeca262f31fac
Michael Wu <wz.wzh@qq.com>
2018-05-15 20:22:22 +0800

    Add a test for pause when async_call, which fails

--- test/Misc/TestRpcService.cpp
+++ test/Misc/TestRpcService.cpp
@@ -30,9 +30,9 @@ BOOST_AUTO_TEST_CASE(Raft) {
              [](Term term, ServerId candidateId,
                 std::size_t lastLogIdx, Term lastLogTerm)
                  -> std::pair<Term, bool> { return {0, 0}; });
-    rpcSrv.async_run(2);
+    rpcSrv.async_run(4);
 
-    const std::size_t N = 5;
+    const std::size_t N = 15;
     const std::int64_t TimeMs = 5000;
 
     std::vector<std::thread> ts(N);
@@ -58,6 +58,51 @@ BOOST_AUTO_TEST_CASE(Raft) {
         t.join();
 
     BOOST_REQUIRE_NO_THROW(rpcSrv.stop());
+}
+
+BOOST_AUTO_TEST_CASE(PauseWhenCalling) {
+	BOOST_TEST_MESSAGE("Misc::RpcService::PauseWhenCalling");
+	using namespace quintet;
+
+	quintet::RpcService rpcSrv;
+	rpcSrv.listen(8000);
+	rpcSrv.bind("TimeConsuming", []() -> bool {
+		std::default_random_engine e{ std::random_device()() };
+		std::uniform_int_distribution<int> uniform_dist(1000, 2000);
+		auto randTime = std::bind(uniform_dist, e);
+		std::this_thread::sleep_for(std::chrono::milliseconds(randTime()));
+		return true;
+	});
+	rpcSrv.async_run(4);
+
+	const size_t callNum = 5;
+	std::vector<std::thread> callThread(callNum);
+	std::vector<std::unique_ptr<rpc::client>> clients;
+	for (size_t i = 0; i < callNum; ++i) {
+		clients.emplace_back(std::make_unique<rpc::client>("localhost", 8000));
+	}
+
+	for (size_t i = 0; i < callNum; ++i) {
+		callThread[i] = std::thread([&]() {
+			clients[i]->async_call("TimeConsuming");
+
+			/*rpc::client c("localhost", 8000);
+			c.async_call("TimeConsuming");
+			std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		*/});
+	}
+	
+	auto pauseThread = std::thread([&]() {
+		rpcSrv.pause();
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		rpcSrv.resume();
+	});
+
+	for (auto &t : callThread) {
+		t.join();
+	}
+	pauseThread.join();
+	BOOST_REQUIRE_NO_THROW(rpcSrv.stop());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
