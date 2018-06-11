fbc6697b2e9b78384b19c3c1c413a8970b6fbdb8
Engineev <ryw_pub@126.com>
2018-06-11 16:43:55 +0800

    RpcService

--- include/quintet/raft/service/rpc/RpcService.h
+++ include/quintet/raft/service/rpc/RpcService.h
@@ -1,8 +1,8 @@
 #ifndef QUINTET_RPCSERVICE_H
 #define QUINTET_RPCSERVICE_H
 
-#include <string>
 #include <memory>
+#include <string>
 
 #include "RaftDefs.h"
 
@@ -15,31 +15,24 @@ public:
 
   ~RpcService();
 
-  void bindAppendEntries(std::function<std::pair<Term, bool>(Term, ServerId, std::size_t, Term, std::vector<LogEntry>, std::size_t)>);
-  void bindRequestVote(std::function<std::pair<Term, bool>(Term, ServerId, std::size_t, Term)>);
+  void bindAppendEntries(
+      std::function<std::pair<Term, bool>(Term, ServerId, size_t, Term,
+                                          std::vector<LogEntry>, size_t)>
+          f);
+  void bindRequestVote(
+      std::function<std::pair<Term, bool>(Term, ServerId, size_t, Term)> f);
 
   void configLogger(const std::string &id);
 
-  /// \brief Change the port to listen on to the given one.
-  /// The original server will be stopped first and the
-  /// functors bound previously will become invalid.
-  ///
-  /// \param port the port to listen on.
-  void listen(Port port);
+  void asyncRun(Port port);
 
-  void async_run(std::size_t worker = 1);
-
-  /// \breif stop the RPC service. All the ongoing RPCs will
-  ///        be finished first.
-  ///
-  /// Currently, please make sure that the RpcService is running
-  /// when stop() is invoked.
+  /// \brief Graceful shutdown.
   void stop();
 
-  void pause() {}
+  void pause();
 
   /// \brief Resume the paused RPC service and notify all the RPCs waiting.
-  void resume() {}
+  void resume();
 
 private:
   struct Impl;
--- src/raft/service/rpc/RpcService.cpp
+++ src/raft/service/rpc/RpcService.cpp
@@ -4,6 +4,12 @@
 #include <utility>
 #include <vector>
 
+#include <boost/atomic/atomic.hpp>
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/shared_mutex.hpp>
+#include <boost/thread/thread.hpp>
+
 #include "service/rpc/RaftRpc.grpc.pb.h"
 #include <grpc++/server.h>
 #include <grpc++/server_builder.h>
@@ -25,8 +31,8 @@ public:
   void bindRequestVote(RequestVoteFunc f) { requestVote = std::move(f); }
 
   grpc::Status AppendEntries(grpc::ServerContext *context,
-                             const quintet::rpc::AppendEntriesMessage *request,
-                             ::quintet::rpc::PbReply *response) override {
+                             const AppendEntriesMessage *request,
+                             PbReply *response) override {
     Term leaderTerm = request->term();
     ServerId leaderId{request->leaderid().addr(),
                       (Port)request->leaderid().port()};
@@ -53,8 +59,8 @@ public:
   }
 
   grpc::Status RequestVote(grpc::ServerContext *context,
-                           const quintet::rpc::RequestVoteMessage *request,
-                           quintet::rpc::PbReply *response) override {
+                           const RequestVoteMessage *request,
+                           PbReply *response) override {
     Term candidateTerm = request->term();
     ServerId candidateId{request->candidateid().addr(),
                          (Port)request->candidateid().port()};
@@ -88,7 +94,108 @@ RpcService::RpcService() : pImpl(std::make_unique<Impl>()) {}
 
 RpcService::~RpcService() = default;
 
-struct RpcService::Impl {};
+struct RpcService::Impl {
+  RaftRpcImpl service;
+  std::unique_ptr<grpc::Server> server;
+  boost::thread runningThread;
+
+  boost::mutex stopping;
+  boost::atomic<std::size_t> numRpcRemaining{0};
+  boost::condition_variable modifiedNumRpcRemaining;
+
+  // sync pause()/resume() and rpc
+  boost::shared_mutex rpcing;
+  boost::mutex paused;
+
+  void asyncRun(Port port) {
+    std::string addr = "0.0.0.0:" + std::to_string(port);
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    server = builder.BuildAndStart();
+    runningThread = boost::thread([this] { server->Wait(); });
+  }
+
+  std::shared_ptr<void> preRpc() {
+    ++numRpcRemaining;
+    boost::unique_lock<boost::mutex> plk(paused);
+    boost::shared_lock<boost::shared_mutex> lk(rpcing);
+    plk.unlock();
+
+    return std::shared_ptr<void>(nullptr, [this](void *) {
+      --numRpcRemaining;
+      modifiedNumRpcRemaining.notify_all();
+    });
+  }
+
+  void bindAppendEntries(
+      std::function<std::pair<Term, bool>(Term, ServerId, std::size_t, Term,
+                                          std::vector<LogEntry>, std::size_t)>
+          f) {
+    service.bindAppendEntries(
+        [this, f](Term term, ServerId leaderId, std::size_t prevLogIdx,
+                  Term prevLogTerm, std::vector<LogEntry> logEntries,
+                  std::size_t commitIdx) {
+          auto defer = preRpc();
+          return f(term, leaderId, prevLogIdx, prevLogTerm, std::move(logEntries),
+            commitIdx);
+        });
+  }
+
+  void bindRequestVote(
+      std::function<std::pair<Term, bool>(Term, ServerId, std::size_t, Term)> f) {
+    service.bindRequestVote([this, f](Term term, ServerId candidateId,
+                                      std::size_t lastLogIdx,
+                                      Term lastLogTerm) {
+      auto defer = preRpc();
+      return f(term, candidateId, lastLogIdx, lastLogTerm);
+    });
+  }
+
+  void pause() {
+    paused.lock();
+    boost::unique_lock<boost::shared_mutex> lk(rpcing);
+  }
+
+  void resume() {
+    paused.unlock();
+  }
+
+  void stop() {
+    boost::unique_lock<boost::mutex> lk(stopping);
+    modifiedNumRpcRemaining.wait(lk, [this] {
+      return !numRpcRemaining;
+    });
+    if (server)
+      server->Shutdown();
+  }
+
+}; // struct RpcService::Impl
+
+} // namespace rpc
+} // namespace quintet
+
+namespace quintet {
+namespace rpc {
+
+void RpcService::asyncRun(Port port) { pImpl->asyncRun(port); }
+
+void RpcService::pause() { pImpl->pause(); }
+
+void RpcService::resume() { pImpl->resume(); }
+
+void RpcService::stop() { pImpl->stop(); }
+
+void RpcService::bindAppendEntries(
+    std::function<std::pair<Term, bool>(Term, ServerId, size_t, Term,
+                                        std::vector<LogEntry>, size_t)>
+    f) {
+  pImpl->bindAppendEntries(std::move(f));
+}
+void RpcService::bindRequestVote(
+    std::function<std::pair<Term, bool>(Term, ServerId, size_t, Term)> f) {
+  pImpl->bindRequestVote(std::move(f));
+}
 
 } // namespace rpc
 } // namespace quintet
