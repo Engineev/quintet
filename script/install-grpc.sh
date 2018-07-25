0d8d86b15bc8353086fc739d237f7f2689ec0ff1
Engineev <ryw_pub@126.com>
2018-07-25 15:42:57 +0800

    grpc !!!

--- script/install-grpc.sh
+++ script/install-grpc.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
 
-export CC=gcc-5
-export CXX=g++-5
-
 git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
-make -j all
+make
 sudo make install
 
 cd third_party/protobuf
