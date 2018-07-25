8c84710e2b938dddb5edae23b6312e8467c75db9
Engineev <ryw_pub@126.com>
2018-07-25 13:12:45 +0800

    travis CE

--- include/quintet/server/actor_types.h
+++ include/quintet/server/actor_types.h
@@ -1,12 +1,15 @@
 #ifndef QUINTET_ACTOR_TYPES_H
 #define QUINTET_ACTOR_TYPES_H
 
-#include <theatre/actor.h>
-#include <theatre/string.h>
+#include <theatre/all.h>
 
 namespace quintet {
 
+namespace tag {
 
-}
+} /* namespace tag */
+
+
+} /* namespace quintet */
 
 #endif //QUINTET_ACTOR_TYPES_H
